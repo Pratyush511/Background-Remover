bca28b37a1f8d548dcd3e7233f7953eab0b203ee
P <86196200+Pratyush511@users.noreply.github.com>
2026-04-04 02:05:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from rembg import remove
+import io
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Background remover API running"}
+
+@app.post("/remove-bg")
+async def remove_bg(file: UploadFile = File(...)):
+    input_data = await file.read()
+    output_data = remove(input_data)
+
+    return StreamingResponse(
+        io.BytesIO(output_data),
+        media_type="image/png"
+    )
